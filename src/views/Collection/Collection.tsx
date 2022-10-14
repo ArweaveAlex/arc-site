074571fba7694a7743b7809da800fc6b78697a29
VinceJuliano <VinceJuliano>
2022-10-14 13:28:49 -0400

    artefact count

--- src/views/Collection/Collection.tsx
+++ src/views/Collection/Collection.tsx
@@ -5,13 +5,29 @@ import { ArweaveCollectionProps } from "@/types";
 import { CollectionHeader } from "./CollectionHeader";
 import { CollectionDetail } from "./CollectionDetail";
 
-import { getTxUrl, formatDate } from "@/util";
+import { getTxUrl, formatDate, getTagValue } from "@/util";
 import * as S from "./styles";
+import React from "react";
+import { LANGUAGE } from "@/language";
 
 export default function _Collection(props: { data: ArweaveCollectionProps }) {
     const arProvider = useARProvder();
+
+    const [data, setData] = React.useState<any>(null);
+
+    React.useEffect(() => {
+        (async function () {
+            setData((await arProvider.getAllArtefactsByPool(props.data.id)).map((element: any) => {
+                return { 
+                    // title: getViewblockLink(getTagValue(element.node.tags, "Uploader-Tx-Id"), getTagValue(element.node.tags, "Artefact-Name")), 
+                    title: getTagValue(element.node.tags, "Artefact-Name"), 
+                    dateCreated: formatDate(getTagValue(element.node.tags, "Created-At"), "ts") 
+                }
+            }));
+        })();
+    }, [])
     
-    return (
+    return data ? (
         <S.Wrapper>
             <CollectionHeader
                 id={props.data.id}
@@ -19,10 +35,10 @@ export default function _Collection(props: { data: ArweaveCollectionProps }) {
                 title={props.data.state.title}
                 description={props.data.state.description}
                 dateCreated={formatDate(props.data.ts, "iso")}
-                artefactCount={props.data.artefacts || 0}
+                artefactCount={data ? data.length : 0}
                 totalContributions={arProvider.getARAmount(props.data.state.totalContributions)}
             />
-            <CollectionDetail collectionData={props.data}/>
+            <CollectionDetail artefactData={data}/>
         </S.Wrapper>
-    )
+    ) : <p>{LANGUAGE.loading}&nbsp;...</p>
 }
--- src/views/Collection/CollectionDetail/CollectionDetail.tsx
+++ src/views/Collection/CollectionDetail/CollectionDetail.tsx
@@ -9,38 +9,16 @@ import { LANGUAGE } from "@/language"
 
 import { ArweaveCollectionProps } from "@/types";
 
-export default function CollectionDetail(props: { collectionData: ArweaveCollectionProps }) {
-    const arProvider = useARProvder();
+export default function CollectionDetail(props: { artefactData: any }) {
 
-    const [data, setData] = React.useState<any>(null);
-
-    // function getViewblockLink(uploaderTxId: string | null, label: string | null) {
-    //     if (!uploaderTxId || !label) {
-    //         return <a target="_blank" href={"#"}>{"None"}</a>
-    //     }
-    //     return <a target="_blank" href={`https://v2.viewblock.io/arweave/tx/${uploaderTxId}`}>{label}</a>
-    // }
-
-    React.useEffect(() => {
-        (async function () {
-            setData((await arProvider.getAllArtefactsByPool(props.collectionData.id)).map((element: any) => {
-                return { 
-                    // title: getViewblockLink(getTagValue(element.node.tags, "Uploader-Tx-Id"), getTagValue(element.node.tags, "Artefact-Name")), 
-                    title: getTagValue(element.node.tags, "Artefact-Name"), 
-                    dateCreated: formatDate(getTagValue(element.node.tags, "Created-At"), "ts") 
-                }
-            }));
-        })();
-    }, [])
-
-    return data ? (
+    return props.artefactData ? (
         <Table
             title={LANGUAGE.artefacts}
             header={{
                 title: { width: "77.5%" },
                 dateCreated: { width: "22.5%" }
             }}
-            data={data}
+            data={props.artefactData}
             recordsPerPage={50}
         />
     ) : <p>{LANGUAGE.loading}&nbsp;...</p>
