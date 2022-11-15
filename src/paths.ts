931827ed6eb3279e51e6d625af4d5b07218a0b42
Nick Juliano <nickjuliano20@gmail.com>
2022-11-15 13:22:17 -0500

    External Discord Link

--- src/paths.ts
+++ src/paths.ts
@@ -13,23 +13,15 @@ export const NAV_PATHS = [
     }
 ]
 
-// export const SOCIAL_PATHS = [
-//     {
-//         name: LANGUAGE.social.twitter, 
-//         href: "https://twitter.com/thealexarchive",
-//         svg: ASSETS.social.twitter
-//     }, 
-//     {
-//         name: LANGUAGE.social.discord, 
-//         href: "#",
-//         svg: ASSETS.social.discord
-//     }
-// ]
-
 export const SOCIAL_PATHS = [
     {
         name: LANGUAGE.social.twitter, 
         href: "https://twitter.com/thealexarchive",
         svg: ASSETS.social.twitter
+    }, 
+    {
+        name: LANGUAGE.social.discord, 
+        href: "https://discord.gg/jRTnwdrxg5",
+        svg: ASSETS.social.discord
     }
 ]
