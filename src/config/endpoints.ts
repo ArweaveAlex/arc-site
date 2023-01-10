6ff570ef506bde9745e441a14ef32737ef8f14a6
VinceJuliano <v.juliano6363@gmail.com>
2023-01-10 15:52:25 -0500

    fix count and recently minted

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -13,3 +13,7 @@ export function getTxEndpoint(txId: string) {
 export function getRedstoneSrcTxEndpoint(contractId: string){
     return `https://gateway.redstone.finance/gateway/contracts-by-source?id=${contractId}&limit=15&page=1`;
 }
+
+export function getRedstoneDescEndpoint(nftSrc: string) {
+    return `https://gateway.redstone.finance/gateway/contracts-by-source?id=${nftSrc}&page=1&sort=desc&limit=5`;
+}
--- src/views/Pool/PoolStatistics/PoolStatistics.tsx
+++ src/views/Pool/PoolStatistics/PoolStatistics.tsx
@@ -1,15 +1,54 @@
-// import { PoolRecentlyMinted } from "./PoolRecentlyMinted";
+import { PoolRecentlyMinted } from "./PoolRecentlyMinted";
 import { PoolContributors } from "./PoolContributors";
 
 import { IProps } from "./types";
 import * as S from "./styles";
 
+import { getRedstoneDescEndpoint } from "config/endpoints";
+import React from "react";
+import { REDUX_TABLES } from "config/redux";
+import { getArtifactsByIds, getArtifactsByPool } from "gql/artifacts";
+import { getTagValue } from "config/utils";
+import { TAGS } from "config";
+import { ArtifactResponseType, GQLResponseType } from "config/types";
+
 export default function PoolStatistics(props: IProps) {
-    // TODO - Cache PoolRecentlyMinted
+    const [detailData, setDetailData] = React.useState<ArtifactResponseType | null>(null);
+
+    React.useEffect(() => {
+        (async function () {
+            if(props.headerData) {
+                let dataForTags = await getArtifactsByPool({
+                    ids: [props.headerData.id],
+                    owner: null,
+                    uploader: props.headerData.state.owner,
+                    cursor: null,
+                    reduxCursor: REDUX_TABLES.poolAll
+                });
+                if(dataForTags && (dataForTags.contracts.length > 0)) {
+                    let nftContractSrc = getTagValue(dataForTags.contracts[0].node.tags, TAGS.keys.contractSrc);
+                    let redstoneContracts = await fetch(getRedstoneDescEndpoint(nftContractSrc));
+                    let json = await redstoneContracts.json();
+                    let ids = json.contracts.map((c: any) => {return c.contractId});
+                    let detailDataFinal = await getArtifactsByIds({
+                        ids: ids,
+                        owner: null,
+                        uploader: null,
+                        cursor: null,
+                        reduxCursor: null
+                    });
+                    setDetailData(detailDataFinal);
+                }
+            }
+        })();
+    }, [])
 
     return (
         <S.Wrapper>
-            {/* <PoolRecentlyMinted data={props.detailData.contracts.slice(0, 5)} /> */}
+            {detailData && 
+                <PoolRecentlyMinted data={detailData.contracts} />
+            }
+
             <PoolContributors data={props.headerData} />
         </S.Wrapper>
     )
