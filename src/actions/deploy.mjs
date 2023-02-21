740eb1d6e6bf6566bfdcad21078903e12ba1282e
Nick Juliano <nickjuliano20@gmail.com>
2023-02-20 22:41:08 -0500

    Remove CRA / Setup node / webpack

--- src/actions/deploy.mjs
+++ src/actions/deploy.mjs
@@ -15,7 +15,7 @@ import { WarpFactory, defaultCacheOptions } from "warp-contracts";
     Buffer.from(process.env.ALEX_DEPLOY_KEY, "base64").toString("utf-8")
   );
 
-  const bundlr = new (Bundlr as any).default(BUNDLR_NODE, "arweave", jwk);
+  const bundlr = new Bundlr.default(BUNDLR_NODE, "arweave", jwk);
   const warp = WarpFactory.custom(arweave, defaultCacheOptions, "mainnet")
     .useArweaveGateway()
     .build();
