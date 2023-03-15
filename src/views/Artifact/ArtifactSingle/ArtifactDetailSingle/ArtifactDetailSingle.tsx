72425e624dcfca2777371fd3f0344ae1189b3019
NickJ202 <nickjuliano20@gmail.com>
2023-03-15 06:55:44 -0400

    fix: update verbiage

--- src/views/Artifact/ArtifactSingle/ArtifactDetailSingle/ArtifactDetailSingle.tsx
+++ src/views/Artifact/ArtifactSingle/ArtifactDetailSingle/ArtifactDetailSingle.tsx
@@ -33,7 +33,7 @@ export default function ArtifactDetailSingle(props: IProps) {
 					<S.InfoData>
 						<S.DataWrapper>
 							<S.DataLine>
-								<S.DataHeader>{LANGUAGE.archivist}:&nbsp;</S.DataHeader>
+								<S.DataHeader>{LANGUAGE.owner}:&nbsp;</S.DataHeader>
 								<Link to={`${urls.libraryAll(props.data.owner!)}`}>{formatAddress(props.data.owner, false)}</Link>
 							</S.DataLine>
 						</S.DataWrapper>
