6339af3677a8d1f8e6def648d33e9ec6bdc0ffd4
NickJ202 <nickjuliano20@gmail.com>
2023-05-10 09:32:05 -0400

    create bundlr web alias

--- webpack.config.js
+++ webpack.config.js
@@ -184,6 +184,7 @@ module.exports = {
 			wrappers: path.resolve(__dirname, 'src/wrappers/'),
 			'asn1.js': path.resolve(__dirname, 'node_modules/asn1.js'),
 			elliptic: path.resolve(__dirname, 'node_modules/elliptic'),
+			'@bundlr-network/client': '@bundlr-network/client/build/cjs/web/bundlr',
 		},
 	},
 	output: {
