9529fd1c42d8f010b580690c45d1a88beb180dfe
NickJ202 <nickjuliano20@gmail.com>
2023-05-12 16:05:15 -0400

    update package check script

--- src/workers/check-packages.mjs
+++ src/workers/check-packages.mjs
@@ -3,11 +3,11 @@ import * as fs from 'fs';
 const packageJson = fs.readFileSync('./package.json');
 const packageData = JSON.parse(packageJson);
 
-console.log('Checking yalc packages ...');
+console.log('Checking packages ...');
 for (let i = 0; i < Object.keys(packageData.dependencies).length; i++) {
 	const dep = Object.keys(packageData.dependencies)[i];
-	if (packageData.dependencies[dep].includes('file:.yalc')) {
-		console.error('Local yalc link found in dependencies - aborting commit');
+	if (packageData.dependencies[dep].includes('file')) {
+		console.error('Local package link found in dependencies - aborting commit');
 		process.exit(1);
 	}
 }
